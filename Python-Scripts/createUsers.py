1d2a5317effffd3ff86dd26c00ed83ecc152ab31
VascoLucas01 <vascolucas01@gmail.com>
2023-06-01 17:37:56 +0100

    Update createUsers.py

--- Python-Scripts/createUsers.py
+++ Python-Scripts/createUsers.py
@@ -2,16 +2,23 @@
 
 ### LIBRARIES
 import boto3
+import sys
 
 def main():
 
+    len = len(sys.argv)
+    if len == 1:
+        print("usage: {0} <name_1> <name_2> ... <name_n>".format(
+            sys.argv[0]
+            )
+        )
+        exit(0)
+        
     iam = boto3.client('iam')
     
-    user_name = input("Enter the user name:\n> ")
-    
-    response = iam.create_user(UserName=user_name)
-    
-    print(response) 
+    for i in len:
+        iam.create_user(UserName=sys.argv[i])
+     
     
 
 if __name__ == "__main__":
