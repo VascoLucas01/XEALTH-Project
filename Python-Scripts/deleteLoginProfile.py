764506c057cd5fcd7fdad07ee39c739976d50ba3
Vasco Coelho de Melo Gouveia Lucas <110473841+VascoLucas01@users.noreply.github.com>
2023-06-01 20:48:36 +0100

    Update deleteLoginProfile.py

--- Python-Scripts/deleteLoginProfile.py
+++ Python-Scripts/deleteLoginProfile.py
@@ -21,7 +21,7 @@ def user_exists(iam, user_name):
     
     
 # Function name: main
-# Purpose      : Deletes the users' login profiles specified
+# Purpose      : Deletes the users' login profile specified
 # Arguments    : none
 # Return       : none   
 def main():
