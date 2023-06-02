5551b4f9720ec2e73f79d3e637db19093d688ae4
VascoLucas01 <vascolucas01@gmail.com>
2023-06-02 20:08:17 +0100

    Move removeUserFromGroup.py to AWS-iam

---
+++ Python-Scripts/AWS-iam/removeUserFromGroup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+
+### LIBRARIES
+from botocore.exceptions import ClientError
+import boto3
+
+
+### FUNCTIONS
+# Function name: user_exists
+# Purpose      : Verify if the user specified exists
+# Arguments    : iam, username
+# Return       : none   
+def user_exists(iam, user_name):
+    try:
+        iam.get_user(UserName=user_name)
+        return True
+    except iam.exceptions.NoSuchEntityException:
+        return False
+    except:
+        return False
+    
+    
+# Function name: group_exists
+# Purpose      : Verify if the group specified exists
+# Arguments    : iam, group_name
+# Return       : none   
+def group_exists(iam, group_name):
+    try:
+        iam.get_group(GroupName=group_name)
+        return True
+    except iam.exceptions.NoSuchEntityException:
+        return False
+    except:
+        return False
+
+
+# Function name: main
+# Purpose      : Prompts the user for a user name and group name in order to remove that user from that group
+# Arguments    : none
+# Return       : none        
+def main():
+    
+    iam = boto3.client('iam')
+
+    user_name = input("Enter the user you want to remove from a group:\n\n> ")
+    group_name = input("\n\nEnter group name:\n\n> ")
+
+    if(user_exists(iam, user_name)):
+        if(group_exists(iam,group_name)):
+            try:
+                iam.remove_user_from_group(
+                    GroupName=group_name,
+                    UserName=user_name
+                )
+            except Exception as e:
+                print(f"Exception type: {type(e)}\n")
+        else:
+            print(f"---- The group {group_name} does not exist ----")
+    else:
+        print(f"---- The user {user_name} does not exist ----")
+            
+
+
+
+if __name__ == "__main__":
+    main()
+
+
