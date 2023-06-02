cfab2b87fcacb57a34387224927e547e24564692
VascoLucas01 <vascolucas01@gmail.com>
2023-06-02 20:29:21 +0100

    Move deleteCustomerGateway.py to AWS-ec2

---
+++ Python-Scripts/AWS-ec2/deleteCustomerGateway.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+### LIBRARIES
+from botocore.exceptions import ClientError
+import boto3
+import sys
+
+
+### FUNCTIONS
+# Function name: main
+# Purpose      : Deletes a customer gateway
+# Arguments    : none
+# Return       : none 
+def main():
+
+    number_of_names = len(sys.argv)
+    if number_of_names != 2:
+        print("usage: {0} <customer_gateway_id>".format(
+            sys.argv[0]
+            )
+        )
+        exit(0)
+    
+    customer_gateway_id = sys.argv[1]
+    
+    
+    ec2 = boto3.client('ec2')
+    
+    try:
+        ec2.delete_customer_gateway(CustomerGatewayId=customer_gateway_id)
+    except Exception as e:
+        print(e)
+        print(f"Exception type: {type(e)}\n")
+        print(f"---- Something went wrong when deleting the customer gateway ----")
+
+
+
+
+    
+if __name__ == "__main__":
+    main()    
+    
