f72652d809d40ad643e0559cbe674754a3fd2f4c
VascoLucas01 <vascolucas01@gmail.com>
2023-06-02 01:27:47 +0100

    Creation of deleteSubnet.py

---
+++ Python-Scripts/deleteSubnet.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+### LIBRARIES
+from botocore.exceptions import ClientError
+import boto3
+import sys
+
+
+
+### FUNCTIONS
+# Function name: main
+# Purpose      : Deletes a subnet
+# Arguments    : none
+# Return       : none 
+def main():
+
+    number_of_names = len(sys.argv)
+    if number_of_names != 2:
+        print("usage: {0} <subnet_id>".format(
+            sys.argv[0]
+            )
+        )
+        exit(0)
+    
+    subnet_id = sys.argv[1]
+    
+    ec2 = boto3.client('ec2')
+    
+    try:
+        subnet = ec2.delete_subnet(SubnetId=subnet_id)
+        
+    except Exception as e:
+        print(e)
+        print(f"Exception type: {type(e)}\n")
+        print(f"---- Something went wrong when deleting the subnet ----")
+            
+            
+    
+if __name__ == "__main__":
+    main()    
+    
